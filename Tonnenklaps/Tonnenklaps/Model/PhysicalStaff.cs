c60f345544e9182eda9bca01217a9dd14a21cf64
Jern_cp <SND\Jern_cp>
2011-02-18 21:34:08 +0000

    PhysicalStaff added

---
+++ Tonnenklaps/Tonnenklaps/Model/PhysicalStaff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Tonnenklaps.Model
+{
+    public class PhysicalStaff
+    {
+        public Color Color
+        {
+            get;
+            set;
+        }
+
+        public bool Destroyed
+        {
+            get;
+            set;
+        }
+    }
+}
