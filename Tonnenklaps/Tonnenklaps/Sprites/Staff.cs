23a121b3d4b918a29b823277b1b072ac52ee127d
Jern_cp <SND\Jern_cp>
2011-02-18 21:28:50 +0000

    Staff added

---
+++ Tonnenklaps/Tonnenklaps/Sprites/Staff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameDev.Sprites;
+using Microsoft.Xna.Framework;
+
+namespace Tonnenklaps.Sprites
+{
+    public class Staff : Sprite
+    {
+        public Staff(Game game, Vector2 startpos) : base(game, startpos)
+        {
+
+        }
+
+
+        protected override GameDev.GraphicUtils.ImageState ResetImageState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
