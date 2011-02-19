bcbaad721fa6c4371b6599f36d0bd9a5f3ab67c2
Jern_cp <jesper@niedermann.dk>
2011-02-19 12:01:43 +0000

    Splashscreen finito

---
+++ Tonnenklaps/Tonnenklaps/Util/GameEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tonnenklaps.Util
+{
+    public static class GameEnvironment
+    {
+        public const int GameWidth = 800;
+        public const int GameHeight = 600;
+
+    }
+}
