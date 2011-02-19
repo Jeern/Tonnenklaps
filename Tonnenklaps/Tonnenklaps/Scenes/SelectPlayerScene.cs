c496bb6cc9cf88956e50046c23c0f4e0a0162043
Jern_cp <jesper@niedermann.dk>
2011-02-19 16:01:27 +0000

    PlayerChoice finished

--- Tonnenklaps/Tonnenklaps/Scenes/SelectPlayerScene.cs
+++ Tonnenklaps/Tonnenklaps/Scenes/SelectPlayerScene.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GameDev.Scenes;
+using GameDev.Utils;
+using GameDev.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Tonnenklaps.Util;
+using Tonnenklaps.Sprites;
 
 namespace Tonnenklaps.Scenes
 {
     public class SelectPlayerScene : StaticScene
     {
         public SelectPlayerScene(string textureFile) : base(textureFile)
-            
         {
+            
+        }
+
+        public override void Initialize()
+        {
+            GameEnvironment.CurrentPlayers = new List<Player>();
+            SetPlayer(PlayerIndex.One);
+            SetPlayer(PlayerIndex.Two);
+            SetPlayer(PlayerIndex.Three);
+            SetPlayer(PlayerIndex.Four);
+            base.Initialize();
+        }
+
+        private void SetPlayer(PlayerIndex playerIndex)
+        {
+            if (GamePad.GetCapabilities(playerIndex).IsConnected)
+            {
+                var player = new Player(Vector2.Zero);
+                player.PlayerIndex = playerIndex;
+                GameEnvironment.CurrentPlayers.Add(player);
+            }
         }
 
     }
---
+++ Tonnenklaps/Tonnenklaps/Sprites/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameDev.Sprites;
+using Microsoft.Xna.Framework;
+using GameDev.GraphicUtils;
+
+namespace Tonnenklaps.Sprites
+{
+    public class Player : Sprite
+    {
+        public Player(Vector2 startPos) : base(startPos)
+        {
+
+        }
+
+        protected override ImageState ResetImageState()
+        {
+            //Todo: iMPLEMENTER
+            throw new NotImplementedException();
+        }
+
+        public PlayerIndex PlayerIndex
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
