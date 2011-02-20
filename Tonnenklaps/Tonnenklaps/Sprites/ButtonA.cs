cb245ebef06c01a45dbd4b56ac5c5f6abe01433c
Jern_cp <jesper@niedermann.dk>
2011-02-20 01:55:55 +0000

    Buttons added

---
+++ Tonnenklaps/Tonnenklaps/Sprites/ButtonA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameDev.Sprites;
+using Microsoft.Xna.Framework;
+using GameDev.GraphicUtils;
+using Microsoft.Xna.Framework.Graphics;
+using GameDev.Utils;
+using Tonnenklaps.Util;
+
+namespace Tonnenklaps.Sprites
+{
+    public class ButtonA : StaticSprite
+    {
+        public ButtonA(Vector2 vector, string text) : base(vector, @"Buttons\ButtonA", text)
+        {
+        }
+
+    }
+}
---
+++ Tonnenklaps/Tonnenklaps/Sprites/ButtonY.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Tonnenklaps.Sprites
+{
+    public class ButtonY : StaticSprite
+    {
+        public ButtonY(Vector2 vector, string text)
+            : base(vector, @"Buttons\ButtonY", text)
+        {
+        }
+
+    }
+}
